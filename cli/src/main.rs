c88e1c571c0edf9ccd192380037510608d54a137
Wei <williamlin910403@gmail.com>
2021-09-10 23:29:25 +0800

    add more function

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,11 +2,19 @@ use crate::util::get_pub;
 mod ticket_finding;
 mod util;
 use std::{env};
+use solana_client::{rpc_client::RpcClient};
+use solana_sdk::{ commitment_config::CommitmentConfig};
+
+
 fn main() {
     let arg: Vec<String> = env::args().collect();
     let pool_id = get_pub(&arg[1]);
+    let rpc_url: String = "https://api.devnet.solana.com".to_string();
+    //change this URL to change Cluster
+    let commitment = CommitmentConfig::confirmed();
+    let rpc_client = RpcClient::new_with_commitment(rpc_url, commitment);
 
-    let tickets = ticket_finding::findtickets(&pool_id);
+    let tickets = ticket_finding::findtickets(&pool_id,rpc_client);
 
     for data in tickets {
         println!("{:?},{:?}", data.0, data.1);
--- cli/src/ticket_finding.rs
+++ cli/src/ticket_finding.rs
@@ -1,28 +1,112 @@
-
+use std::borrow::Borrow;
 use solana_account_decoder::UiAccountEncoding;
-
-
 use solana_client::{rpc_client::RpcClient,rpc_config::{RpcProgramAccountsConfig,RpcAccountInfoConfig},rpc_filter::{RpcFilterType,Memcmp,MemcmpEncodedBytes}};
-use solana_program::{  program_pack::{ Pack, }};
+use solana_program::{program_pack::{ Pack}};
 
 
 
 
-use solana_sdk::{ commitment_config::CommitmentConfig,   pubkey::Pubkey,  };
+use solana_sdk::{ commitment_config::CommitmentConfig, instruction::{AccountMeta, Instruction}, pubkey::Pubkey, signature::{Keypair, Signer}, system_instruction::create_account,};
 
-use crate::util::{get_pub,Ticket};
-
-
+use crate::util::{get_pub,Pool,Ticket};
 
 
 
-pub fn findtickets(pool_id:&Pubkey)-> Vec<(u64,Pubkey)>{
-    let mut ticket_data:Vec<(u64,Pubkey)> = vec!();
+pub fn init_pool(
+    manger: &Pubkey,
+    fee_receiver: &Pubkey,
+    price: u64,
+    fee: u8,
+    total_amount: u64,
+    connection: RpcClient,
+) -> (Vec<Instruction>, Keypair ) {
     let ticket_program_id = get_pub("AUaGuQhpjttMdBmejoboMoUMrpcxNHZsT44C6jupLYNP");
+    let mut ins: Vec<Instruction> = vec![];
+    let pool_key = Keypair::new();
+    let lamport_needed = connection
+        .get_minimum_balance_for_rent_exemption(95)
+        .unwrap();
+    let create_account_ins = create_account(
+        manger,
+        &pool_key.pubkey(),
+        lamport_needed,
+        90,
+        &ticket_program_id,
+    );
+    ins.push(create_account_ins);
+    let mut data: Vec<u8> = vec![];
+    let mut keys: Vec<AccountMeta> = vec![];
+    keys.push(AccountMeta::new(pool_key.pubkey(), true));
+    keys.push(AccountMeta::new(*manger.borrow(), true));
+    keys.push(AccountMeta::new(*fee_receiver.borrow(), false));
 
+    data.push(0);
+    data.extend_from_slice(&price.to_le_bytes());
+    data.extend_from_slice(&fee.to_le_bytes());
+    data.extend_from_slice(&total_amount.to_le_bytes());
+    let init_pool_ins = Instruction {
+        program_id: ticket_program_id,
+        data: data,
+        accounts: keys,
+    };
+    ins.push(init_pool_ins);
+
+    (ins, pool_key)
+}
+pub fn buy(pool_id:&Pubkey,buyer:&Pubkey,connection: RpcClient,)-> (Vec<Instruction>, Keypair){
+    let ticket_program_id = get_pub("AUaGuQhpjttMdBmejoboMoUMrpcxNHZsT44C6jupLYNP");
+    let mut ins: Vec<Instruction> = vec![];
+    let ticket_key = Keypair::new();
+    let lamport_needed = connection
+        .get_minimum_balance_for_rent_exemption(73)
+        .unwrap();
+    let create_account_ins = create_account(
+        buyer,
+        &ticket_key.pubkey(),
+        lamport_needed,
+        73,
+        &ticket_program_id,
+    );
+    ins.push(create_account_ins);
+
+    let pool_info =connection.get_account_data(pool_id).unwrap();
+    let poo_info_ser = Pool::unpack_unchecked(&pool_info).unwrap();
+    let mut data: Vec<u8> = vec![];
+    let mut keys: Vec<AccountMeta> = vec![];
+    keys.push(AccountMeta::new(pool_id.clone(),false));
+    keys.push(AccountMeta::new(poo_info_ser.manager.clone(),false));
+    keys.push(AccountMeta::new(poo_info_ser.fee_reciever.clone(),false));
+    keys.push(AccountMeta::new(ticket_key.pubkey().clone(),true));
+    keys.push(AccountMeta::new(buyer.clone(),true));
+    keys.push(AccountMeta::new_readonly(get_pub("11111111111111111111111111111111"), false));
+    data.push(1);
+    let buy_ins = Instruction{
+        program_id:ticket_program_id.clone(),
+        data:data,
+        accounts:keys,
+    };
+    ins.push(buy_ins);
+
+
+    (ins, ticket_key)
+}
+
+
+pub fn find_pool(pool_id:&Pubkey) -> Pool{
     let rpc_url: String = "https://api.devnet.solana.com".to_string();
     let commitment = CommitmentConfig::confirmed();
     let rpc_client = RpcClient::new_with_commitment(rpc_url, commitment);
+    
+    let pool_info =rpc_client.get_account_data(pool_id).unwrap();
+    let poo_info_ser = Pool::unpack_unchecked(&pool_info).unwrap();
+    poo_info_ser
+
+}
+
+pub fn findtickets(pool_id:&Pubkey, connection: RpcClient)-> Vec<(u64,Pubkey)>{
+    let mut ticket_data:Vec<(u64,Pubkey)> = vec!();
+    let ticket_program_id = get_pub("AUaGuQhpjttMdBmejoboMoUMrpcxNHZsT44C6jupLYNP");
+
     
 
 
@@ -42,7 +126,7 @@ pub fn findtickets(pool_id:&Pubkey)-> Vec<(u64,Pubkey)>{
         with_context:None
     };
 
-    let  accounts = rpc_client.get_program_accounts_with_config(&ticket_program_id,config).unwrap();
+    let  accounts = connection.get_program_accounts_with_config(&ticket_program_id,config).unwrap();
     //println!("{:?}",accounts);
     for data in accounts {
         let account = data.1;
