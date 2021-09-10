ce1ce8b73b129843f2f857f982ea6fa100842194
Wei <williamlin910403@gmail.com>
2021-09-10 23:03:12 +0800

    change a file name

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,12 +1,12 @@
 use crate::util::get_pub;
-mod ticket_buying;
+mod ticket_finding;
 mod util;
 use std::{env};
 fn main() {
     let arg: Vec<String> = env::args().collect();
     let pool_id = get_pub(&arg[1]);
 
-    let tickets = ticket_buying::findtickets(&pool_id);
+    let tickets = ticket_finding::findtickets(&pool_id);
 
     for data in tickets {
         println!("{:?},{:?}", data.0, data.1);
